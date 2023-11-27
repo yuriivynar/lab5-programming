61567ce2d4d3de7fda32cddc080a07286de1c557
yuriivynar <151685418+yuriivynar@users.noreply.github.com>
2023-11-27 16:27:46 +0200

    Edited the code according to the standards PEP 8  lab5.py

--- lab5.py
+++ lab5.py
@@ -1,35 +1,43 @@
 class TimeConverter:
+    """
+    create class TimeConverter
+    """
 
     def __init__(self, time = ""):
+        """
+        Created initialisator
+        """
         self.time = time
-   """
-   create initialisator
-   """
-    def outputConvertedTime(self):
-      if ":" in self.time:
-         hours, minutes, seconds = map(int, self.time.split(':'))
-         total_seconds = hours * 3600 + minutes * 60 + seconds
-         return total_seconds
-      else:
-         total_seconds = int(self.time)
-         hours, remainder = divmod(total_seconds, 3600)
-         minutes, seconds = divmod(remainder, 60)
-         return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
-     """
-     create method which convert time
-     """
-    def __del__(self): #destructor
+
+    def output_converted_time(self):
+        """
+        Converted time    
+        """
+        if ":" in self.time:
+            hours, minutes, seconds = map (int, self.time.split(':'))
+            total_seconds = hours * 3600 + minutes * 60 + seconds
+            return total_seconds
+        total_seconds = int(self.time)
+        hours, remainder = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+    def __del__(self): 
+        """
+        Deleted constructot
+        """
         print(f"TimeConverter {self.time} is destructed")
 
 def main():
-    time_converter = TimeConverter("15:47:15")
-    seconds = time_converter.outputConvertedTime()
-    print(f"{time_converter.time} = {seconds} секунд")
+    """
+    Created method, which have input parameters
+    """
+    time_converter1 = TimeConverter("15:47:15")
+    seconds = time_converter1.output_converted_time()
+    print(f"{time_converter1.time} = {seconds} секунд")
 
-    time_converter = TimeConverter("156231")
-    seconds = time_converter.outputConvertedTime()
-    print(f"{time_converter.time} секунд = {seconds}")
-"""
-the method sets the parameters that will be converted
-"""
+    time_converter2 = TimeConverter("156231")
+    seconds = time_converter2.output_converted_time()
+    print(f"{time_converter2.time} секунд = {seconds}")
+
 main()
